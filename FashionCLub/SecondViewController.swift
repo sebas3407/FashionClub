f5a27d596281d3705f6458c94fbde1933b787a02
Sebastian Ortiz <sortizv@indra.es>
2020-02-04 16:58:14 +0100

    Upload initial project

---
+++ FashionCLub/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FashionCLub
+//
+//  Created by macbookindra on 04/02/2020.
+//  Copyright © 2020 macbookindra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
