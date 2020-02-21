468d39d60bff6bad610621d4dbdbd75ff15d69c7
Sebastian Ortiz <sebasortiz2000@gmail.com>
2020-02-21 10:49:56 +0100

    Add base for register view controller

--- FashionCLub/Controllers/RegisterViewController.swift
+++ FashionCLub/Controllers/RegisterViewController.swift
@@ -9,14 +9,39 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var et_email: UITextField!
+    @IBOutlet weak var et_password: UITextField!
+    @IBOutlet weak var et_repeatPassword: UITextField!
+    
+    var mail : String = ""
+    var password : String = ""
+    var repeatPassword : String = ""
+    var gender : String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
+    @IBAction func register(_ sender: Any) {
+        
+        if(isTheSamePassword(password: self.password, repeatPassword: self.password)){
+            createNewUser()
+        }
+        else{
+            //las contraseñas no concuerdan
+        }
+    }
+    
+    func isTheSamePassword(password : String, repeatPassword : String) -> Bool {
+        return password == repeatPassword
+    }
+    
+    func createNewUser(){
+        
+    }
     /*
     // MARK: - Navigation
 
