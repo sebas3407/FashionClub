2a5d76c95c5be44f3c1274c72dcb237ba4b2a239
Sebastian Ortiz <sebasortiz2000@gmail.com>
2020-02-25 23:07:35 +0100

    Delete useless code

--- FashionCLub/Extensions/TextFIeldExtensions.swift
+++ FashionCLub/Extensions/TextFIeldExtensions.swift
@@ -13,20 +13,12 @@ public extension UITextField {
     func setBottomBorder(bottomColor : CGColor) {
         let border = CALayer()
         let width = CGFloat(1.0)
-        border.borderColor = UIColor.init(displayP3Red: 0.86, green: 0.86, blue: 0.89, alpha: 1).cgColor
+        border.borderColor = bottomColor
+       // border.borderColor = UIColor.init(displayP3Red: 0.86, green: 0.86, blue: 0.89, alpha: 1).cgColor
         border.borderWidth = 4
         border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: self.frame.size.height)
         border.borderWidth = width
         self.layer.addSublayer(border)
         self.layer.masksToBounds = true
-    }
-}
-
-public extension UIButton {
-    func setBorder(borderColor : CGColor) {
-        self.backgroundColor = .clear
-        self.layer.cornerRadius = 27
-        self.layer.borderWidth = 2
-        self.layer.borderColor = borderColor
     }
 }
